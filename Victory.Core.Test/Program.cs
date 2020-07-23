ec0644bd4d9f30b140fbba5c102d2efeee185ef6
demon28 <274733956@qq.com>
2020-07-23 14:54:24 +0800

    开发

---
+++ Victory.Core.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Victory.Core.Models;
+
+namespace Victory.Core.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PageModel page = new PageModel();
+
+            page.PageSize = 10;
+            page.TotalCount = 101;
+
+
+            Console.WriteLine("Hello World!"+ page.ToTalPage);
+        }
+    }
+}
